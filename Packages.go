49068e62add6bb8a68af7f860150e911bf61048b
Aditya Gummididala <adityagvssss@gmail.com>
2025-07-03 00:23:25 +0530

    Create Packages.go

---
+++ Packages.go
@@ -0,0 +1,18 @@
+// All the programs in go lang are made up of packages
+package main //programs start running in package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Here we have imported fmt and math/rand packages; fmt provides us with formatted input output operations where as math/rand provides pseduo random number generators
+func main() { //pseudo random number is a number that isn't random but it is random enough for most purposes
+	fmt.Println("This is a number", rand.Intn(5))
+}
+
+// this is a single linecomment
+
+/*
+This is a mutliline comment
+*/
