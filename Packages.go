eb3fe84caed902d43b58f3e47dfad2efe98da2eb
Aditya Gummididala <adityagvssss@gmail.com>
2025-07-03 00:26:04 +0530

    Update Packages.go

--- Packages.go
+++ Packages.go
@@ -3,12 +3,14 @@ package main //programs start running in package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 // Here we have imported fmt and math/rand packages; fmt provides us with formatted input output operations where as math/rand provides pseduo random number generators
 func main() { //pseudo random number is a number that isn't random but it is random enough for most purposes
 	fmt.Println("This is a number", rand.Intn(5))
+	fmt.Printf("Now you have %g problems. \n", math.Sqrt(4))
 }
 
 // this is a single linecomment
